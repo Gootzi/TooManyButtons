aafbdf34c6baba2745eba62d3582377d0280263e
Gootzi <124401935+Gootzi@users.noreply.github.com>
2023-02-03 23:01:52 -0800

    Update saving.js

--- saving.js
+++ saving.js
@@ -2,9 +2,9 @@ function saveData() {
     localStorage.setItem("TMBsavedata",game.points.toString() + "|" + game.timestore.stored)
 }
 function loadData() {
-    const splitSave = text.split("|");
-    game.points = new OmegaNum(localStorage.getItem("TMBsavedata")[0])
-    game.timestore.stored = new OmegaNum(localStorage.getItem("TMBsavedata")[1])
+    const splitSave = localStorage.getItem("TMBsavedata").split("|");
+    game.points = new OmegaNum(splitSave[0])
+    game.timestore.stored = new OmegaNum(splitSave[1])
 }
 loadData()
 setInterval(saveData,5000)
