34d3f9903ab2c58182813e6e362b9a5454d26b4a
Gootzi <124401935+Gootzi@users.noreply.github.com>
2023-02-03 23:39:55 -0800

    Update saving.js

--- saving.js
+++ saving.js
@@ -1,10 +1,12 @@
 function saveData() {
-    localStorage.setItem("TMBsavedata",game.points.toString() + "|" + game.timestore.stored)
+    localStorage.setItem("TMBsavedata",game.points.toString() + "|" + game.timestore.stored + "|" + game.autobuy.autobuyerPoints.amount + "|" + game.autobuy.autobuyerPoints.cost)
 }
 function loadData() {
     const splitSave = localStorage.getItem("TMBsavedata").split("|");
     game.points = new OmegaNum(splitSave[0])
     game.timestore.stored = new OmegaNum(splitSave[1])
+    game.autobuy.autobuyerPoints.amount = new OmegaNum(splitSave[2])
+    game.autobuy.autobuyerPoints.cost = new OmegaNum(splitSave[3])
 }
 function clearData() {
     localStorage.removeItem("TMBsavedata")
