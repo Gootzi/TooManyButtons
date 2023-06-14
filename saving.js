ecfcae51db2f9f3c6a9bb59c98c56945ae21cfce
Gootzi <124401935+Gootzi@users.noreply.github.com>
2023-06-13 21:15:44 -0700

    Update saving.js
    i am Going to implode.

--- saving.js
+++ saving.js
@@ -1,3 +1,22 @@
+function doMap(g) {
+    // I hate programming this.
+    var myObject = g;
+
+    for (var key in myObject) {
+        if (myObject.hasOwnProperty(key)) {
+            myObject[key] = new OmegaNum(myObject[key]);
+        }
+        if (typeof key === "object") {
+            doMap(g)
+        }
+    }
+}
+
+console.log(myObject);
+// { 'a': 2, 'b': 4, 'c': 6 }
+}
+
+
 function saveData() {
     var saveString = JSON.stringify(game);
     localStorage.setItem("TMBsavedata",saveString);
@@ -6,7 +25,7 @@ function loadData() {
     var saveString = localStorage.getItem("TMBsavedata");
     var toload = JSON.parse(saveString);
     console.log(saveString);
-    var saveTable = toload;
+    var saveTable = doMap(toload);
     game = toload;
 }
 function clearData() {
